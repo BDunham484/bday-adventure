61cd4cda1a190cbfab8572bf97fab5ec9fd6f7d9
ThereWolf <bdunham484@gmail.com>
2023-06-19 19:23:35 -0500

    add router

--- bdy-adventure/src/App.js
+++ bdy-adventure/src/App.js
@@ -1,9 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          <div>
+            <div>
+              {/* <Header /> */}
+              <main>
+                <Routes>
+                  {/* <Route
+                    path="/"
+                    element={<Home />}
+                  /> */}
+                  {/* <Route
+                    path="/login"
+                    element={<Login />}
+                  /> */}
+                  {/* <Route
+                    path="/signup"
+                    element={<Signup />}
+                  /> */}
+                  {/* <Route path="/profile">
+                    <Route path=":username" element={<Profile />} />
+                    <Route path="" element={<Profile />} />
+                  </Route> */}
+                  {/* <Route path="/show/:artists" element={<Show />} /> */}
+                  {/* <Route path="/venue/:venueName" element={<ShowsByVenue />} /> */}
+                  {/* <Route
+                    path="*"
+                    element={<NoMatch />}
+                  /> */}
+                </Routes>
+              </main>
+            </div>
+            {/* <Footer /> */}
+          </div>
+        </Router>
     </div>
   );
 }
